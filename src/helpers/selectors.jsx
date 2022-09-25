a13303ed128617f76e5fd8947ecba053ed406494
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-09-25 05:22:12 -0300

    Remove old comments

--- src/helpers/selectors.jsx
+++ src/helpers/selectors.jsx
@@ -1,10 +1,7 @@
 /**
  * @param {string} element
- * @param {*} baseElement - HTML | Node Element
  * @return {HTMLElement} s - HTMLElement
- * @return {HTMLElement} sEl - HTMLElement
  * @return {NodeListOf} sAll - NodeListOf
- * @return {NodeListOf} sElAll - NodeListOf
  **/
 
 export const s = (element) => document.querySelector(element);
