8697943285dd1e90c2ab33a8076bc73b2630017a
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-03 18:52:34 -0300

    Adding Docker

---
+++ scripts/node/mount.cjs
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+const fs = require('fs');
+const { EOL } = require('os');
+const { execSync: exec } = require('child_process');
+const [, , ...args] = process.argv;
+
+const force = args.includes('--force');
+
+(() => {
+   const ignoreList = fs.readFileSync(resolve('docker/.dockerignore'), 'utf-8').split(EOL);
+   const excludeList = [];
+
+   ignoreList.forEach((toIgnore) => {
+      const path = toIgnore?.trim().replace(/^\.\//g, '');
+
+      if (path?.length === 0 || /^(\s+)?#/g.test(path)) return;
+
+      excludeList.push(`--exclude=${path}`);
+   });
+
+   if (force) exec('rm -rf docker/app');
+   exec(['mkdir -p docker/app', `rsync -av ${excludeList.join(' ')} ./ docker/app/`].join(' && '));
+})();
+
+(() => {
+   const packageJSON = JSON.parse(fs.readFileSync(resolve('package.json'), 'utf-8'));
+
+   // Uninstall React devlopment dependecies
+   const reactDependecies = ['@fvilers/disable-react-devtools', 'react', 'react-dom', 'react-router-dom'];
+
+   reactDependecies.forEach((dependency) => {
+      if (packageJSON.dependencies.hasOwnProperty(dependency)) delete packageJSON.dependencies[dependency];
+   });
+
+   for (const key in packageJSON.dependencies) {
+      packageJSON.dependencies[key] = packageJSON.dependencies[key].replace(/[^0-9.]/g, '');
+   }
+
+   // Mount clean package.json
+   const newPackageJSON = {
+      dependencies: packageJSON.dependencies,
+   };
+
+   fs.writeFileSync('./docker/app/package.json', JSON.stringify(newPackageJSON, null, 3));
+   exec('cd ./docker/app && npm i --ignore-scripts');
+   exec('rm -rf ./dist');
+})();
