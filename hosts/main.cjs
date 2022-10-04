07debedc0ef138fe0adce3718c3a0e396398bc64
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-03 22:53:00 -0300

    Auto update to docker

--- hosts/main.cjs
+++ hosts/main.cjs
@@ -1,8 +1,11 @@
 const express = require('express');
+const { json, urlencoded } = require('body-parser');
 const { resolve } = require('path');
 
 const app = express();
 
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(express.static(resolve('./dist')));
 app.get('*', (req, res) => res.sendFile(resolve('./dist/index.html')));
 
---
+++ scripts/node/watch.cjs
@@ -0,0 +1,33 @@
+const watch = require('node-watch').default;
+const { exec } = require('child_process');
+
+const watcher = watch('./', { recursive: true });
+
+const events = {
+   update: (file) => {
+      exec(`rsync -av ${file} docker/app/${file}`);
+      console.log(`\x1b[1m\x1b[32mUpdated:\x1b[0m ./${file} \x1b[32m➜\x1b[0m ./docker/app/${file}`);
+   },
+   remove: (file) => {
+      exec(`rm -rf docker/app/${file}`);
+      console.log(`\x1b[1m\x1b[31mRemoved:\x1b[0m ./${file} \x1b[31m➜\x1b[0m ./docker/app/${file}`);
+   },
+};
+
+watcher.on('change', (event, file) => {
+   const blacklist = {
+      dot: /\.(git|github|vscode|dockerignore|DS_Store|eslintrcjs|gitignore|prettierignore|prettierrc|stackblitzrc)/gim,
+      start: /^(bash|docker|node_modules|scripts|src|docker-compose.yml|Dockerfile|vite.config.js|index.html)/gim,
+      type: /(.+)?\.(md)/gim,
+      custom: /package(.+)?\.json/gim,
+      test: (file) =>
+         blacklist.dot.test(file) ||
+         blacklist.start.test(file) ||
+         blacklist.type.test(file) ||
+         blacklist.custom.test(file),
+   };
+
+   if (blacklist.test(file)) return;
+
+   events[event](file);
+});
