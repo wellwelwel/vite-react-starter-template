2ed33049555f7235ea478762597ced7e8dc0a018
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-09 00:46:16 -0300

    Removing console

--- hosts/main/app.cjs
+++ hosts/main/app.cjs
@@ -26,11 +26,6 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-   if (req?.session?.views) req.session.views++;
-   else req.session.views = 1;
-
-   console.log(req.session.views);
-
    if (trustedHosts.includes(req.headers.host.split(':').shift())) next();
    else res.status(404).send('');
 });
