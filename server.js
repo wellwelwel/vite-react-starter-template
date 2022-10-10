5e760873992e70d9b862057e53adc6f776c0984f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-10 19:46:10 -0300

    Changing docker volume to readonly

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ import server from './hosts/main/app.js';
 
 dotenv();
 
-const port = process.env.PORT;
-const hosts = JSON.parse(process.env.TRUSTED_HOSTS).map((host) => `    ➜ \x1b[34mhttp://${host}:${port}/\x1b[0m`);
+(() => {
+   const port = process.env.PORT;
+   const hosts = JSON.parse(process.env.TRUSTED_HOSTS).map((host) => `    ➜ \x1b[34mhttp://${host}:${port}/\x1b[0m`);
 
-server.listen(port, () => {
-   console.log(
-      [
-         `\n\n`,
-         `\x1b[0m\x1b[1mExpress \x1b[32m+\x1b[0m\x1b[1m Nodemon\x1b[0m`,
-         `\n\n`,
-         `  \x1b[1m🚀  Listening\x1b[0m in:`,
-         `\n\n`,
-         hosts.join('\n'),
-         `\n\n`,
-      ].join('')
-   );
-});
+   server.listen(port, () => {
+      console.log(
+         [
+            `\n\n`,
+            `\x1b[0m\x1b[1mExpress \x1b[32m+\x1b[0m\x1b[1m Nodemon\x1b[0m`,
+            `\n\n`,
+            `  \x1b[1m🚀  Listening\x1b[0m in:`,
+            `\n\n`,
+            hosts.join('\n'),
+            `\n\n`,
+         ].join('')
+      );
+   });
+})();
