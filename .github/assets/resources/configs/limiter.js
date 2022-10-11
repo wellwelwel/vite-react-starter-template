7c293b52ad14d43127ae2d72a7ac6bdcf9300baa
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-11 11:15:50 -0300

    Updating init:clean resources

---
+++ .github/assets/resources/configs/limiter.js
@@ -0,0 +1,29 @@
+import rateLimit from 'express-rate-limit';
+import setTime from '../helpers/setTime.js';
+
+const setLimiter = (times = 100, perMilliseconds = '5s') => {
+   return rateLimit({
+      windowMs: setTime(perMilliseconds),
+      max: times,
+      standardHeaders: true,
+      legacyHeaders: false,
+   });
+};
+
+const limiter = {
+   /* By pages */
+   main: setLimiter(10, '2s'),
+
+   /* By size */
+   small: setLimiter(35, '2s'),
+   medium: setLimiter(25, '4s'),
+   large: setLimiter(15, '8s'),
+
+   /* Resources */
+   images: setLimiter(60, '3s'),
+
+   errors: setLimiter(4, '2s'),
+   default: setLimiter(),
+};
+
+export default limiter;
---
+++ .github/assets/resources/hosts/main/app.js
@@ -0,0 +1,61 @@
+import { config as dotenv } from 'dotenv';
+import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import limiter from '../../configs/limiter.js';
+import reactApp from './routes/reactApp.js';
+
+dotenv();
+
+const app = express();
+const secret = process.env.SESSION_SECRET;
+const trustedHosts = JSON.parse(process.env.TRUSTED_HOSTS);
+
+app.set('trust proxy', 1);
+
+app.use(
+   helmet({
+      contentSecurityPolicy: false,
+   })
+);
+
+/* Rate Limit */
+app.use((req, res, next) => {
+   const { url } = req;
+
+   /* Resources */
+   if (/\.ico$/.test(url)) limiter.default(req, res, next);
+   else if (/\.(css|json|js)$/.test(url)) limiter.small(req, res, next);
+   else if (/assets/.test(url)) limiter.images(req, res, next);
+   // Default
+   else limiter.default(req, res, next);
+});
+
+app.use(
+   session({
+      secret: secret,
+      cookie: {
+         httpOnly: true,
+         maxAge: 1000 * 60 * 30,
+      },
+      resave: false,
+      saveUninitialized: false,
+   })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Global middleware */
+app.use((req, res, next) => {
+   const host = req.headers.host.split(':')[0];
+
+   if (trustedHosts.includes(host)) next();
+   else res.status(404).send('');
+});
+
+/* Routes */
+app.use(reactApp);
+
+export default app;
