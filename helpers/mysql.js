af354eff456a10bc96900682a15ac9a943d51680
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-10-14 03:32:44 -0300

    Catching errors

--- helpers/mysql.js
+++ helpers/mysql.js
@@ -60,87 +60,119 @@ const setInsertValues = (parentArray) => {
 export default class MySQL {
    constructor() {
       this.connect = async () => {
-         if (!this?.connection) this.connection = await mysql.createConnection(login);
+         try {
+            if (!this?.connection) this.connection = await mysql.createConnection(login);
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
       };
 
       this.select = async (options = { ...defaultOptions.select }) => {
-         await this.connect();
+         try {
+            await this.connect();
 
-         const defaults = { ...defaultOptions.select, ...options };
-         const { table, params } = defaults;
+            const defaults = { ...defaultOptions.select, ...options };
+            const { table, params } = defaults;
 
-         const columns =
-            typeof defaults.columns === 'string'
-               ? defaults.columns
-               : defaults.columns.map((column) => `\`${column}\``).join(', ');
-         const where = defaults.where ? ` WHERE ${defaults.where}` : '';
-         const orderBy = defaults.orderBy[0]
-            ? ` ORDER BY \`${defaults.orderBy[0]}\` ${defaults?.orderBy[1] || 'ASC'}`
-            : '';
-         const limit = defaults.limit ? ` LIMIT ${defaults.limit}` : '';
+            const columns =
+               typeof defaults.columns === 'string'
+                  ? defaults.columns
+                  : defaults.columns.map((column) => `\`${column}\``).join(', ');
+            const where = defaults.where ? ` WHERE ${defaults.where}` : '';
+            const orderBy = defaults.orderBy[0]
+               ? ` ORDER BY \`${defaults.orderBy[0]}\` ${defaults?.orderBy[1] || 'ASC'}`
+               : '';
+            const limit = defaults.limit ? ` LIMIT ${defaults.limit}` : '';
 
-         const query = `SELECT ${columns} FROM \`${table}\`${where}${orderBy}${limit};`;
-         const [rows] = await this.connection.execute(query, params);
+            const query = `SELECT ${columns} FROM \`${table}\`${where}${orderBy}${limit};`;
+            const [rows] = await this.connection.execute(query, params);
 
-         this.verbose && console.log(query, params);
+            this.verbose && console.log(query, params);
 
-         if (defaults.limit === 1) return rows[0] || false;
-         return rows || false;
+            if (defaults.limit === 1) return rows[0] || false;
+            return rows || false;
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
       };
 
       this.insert = async (options = { ...defaultOptions.insert }) => {
-         await this.connect();
+         try {
+            await this.connect();
 
-         const defaults = { ...defaultOptions.insert, ...options };
-         const { table } = defaults;
+            const defaults = { ...defaultOptions.insert, ...options };
+            const { table } = defaults;
 
-         const { values, params } = setInsertValues(defaults.values);
+            const { values, params } = setInsertValues(defaults.values);
 
-         const columns =
-            defaults?.columns?.length > 0
-               ? ' (' + defaults.columns.map((column) => `\`${column}\``).join(', ') + ')'
-               : '';
-         const query = `INSERT INTO \`${table}\`${columns} VALUES ${values.join(', ')};`;
+            const columns =
+               defaults?.columns?.length > 0
+                  ? ' (' + defaults.columns.map((column) => `\`${column}\``).join(', ') + ')'
+                  : '';
+            const query = `INSERT INTO \`${table}\`${columns} VALUES ${values.join(', ')};`;
 
-         this.verbose && console.log(query, params);
+            this.verbose && console.log(query, params);
 
-         const [results] = await this.connection.execute(query, params);
+            const [results] = await this.connection.execute(query, params);
 
-         return results?.insertId || false;
+            return results?.insertId || false;
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
       };
 
       this.update = async (options = { ...defaultOptions.update }) => {
-         await this.connect();
+         try {
+            await this.connect();
 
-         const defaults = { ...defaultOptions.update, ...options };
-         const { table } = defaults;
+            const defaults = { ...defaultOptions.update, ...options };
+            const { table } = defaults;
 
-         const setParams = [];
-         const set = forceValuesToArray(defaults.set)
-            .map(([column, value]) => {
-               setParams.push(value);
-               return `\`${column}\` = ?`;
-            })
-            .join(', ');
-         const where = defaults.where ? ` WHERE ${defaults.where}` : '';
-         const limit = defaults.limit ? ` LIMIT ${defaults.limit}` : '';
-         const params = [...setParams, ...defaults.params];
+            const setParams = [];
+            const set = forceValuesToArray(defaults.set)
+               .map(([column, value]) => {
+                  setParams.push(value);
+                  return `\`${column}\` = ?`;
+               })
+               .join(', ');
+            const where = defaults.where ? ` WHERE ${defaults.where}` : '';
+            const limit = defaults.limit ? ` LIMIT ${defaults.limit}` : '';
+            const params = [...setParams, ...defaults.params];
 
-         const query = `UPDATE \`${table}\` SET ${set}${where}${limit};`;
-         const [results] = await this.connection.execute(query, params);
+            const query = `UPDATE \`${table}\` SET ${set}${where}${limit};`;
+            const [results] = await this.connection.execute(query, params);
 
-         this.verbose && console.log(query, params);
+            this.verbose && console.log(query, params);
 
-         return results?.affectedRows || false;
+            return results?.affectedRows || false;
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
       };
 
       this.execute = async (query, params = null) => {
-         await this.connect();
+         try {
+            await this.connect();
 
-         return (await this.connection.execute(query, params)) || false;
+            return (await this.connection.execute(query, params)) || false;
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
       };
 
-      this.end = () => this.connection.end();
+      this.end = async () => {
+         try {
+            await this.connection.end();
+         } catch (error) {
+            this.verbose && console.error(error);
+            return false;
+         }
+      };
 
       this.verbose = false;
    }
